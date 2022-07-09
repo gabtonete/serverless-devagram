87b4d0651380177aa77c01d48c5fd7cf799cb150
Gabriel Tonete <gabtopi77@gmail.com>
2022-07-09 16:36:48 -0300

    cognito user email confirmation

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -1,7 +1,8 @@
 import { APIGatewayEvent, Handler } from "aws-lambda";
 import { emailRegex, passwordRegex } from "../constants/Regexes";
 import { CognitoServices } from "../services/CognitoServices";
-import { UserRegisterRequest } from "../types/UserRegisterRequest";
+import { ConfirmUserEmailRequest } from "../types/auth/ConfirmUserEmailRequest";
+import { UserRegisterRequest } from "../types/auth/UserRegisterRequest";
 import { DefaultJsonResponse, formatJSONResponse } from "../utils/DefaultGatewayResponse";
 
 export const register: Handler = async (event: APIGatewayEvent): Promise<DefaultJsonResponse> => {
@@ -34,10 +35,42 @@ export const register: Handler = async (event: APIGatewayEvent): Promise<Default
         }
 
         await new CognitoServices(USER_POOL_ID, USER_CLIENT_ID).signUp(email, password);
-        
+
         return formatJSONResponse(200, 'Usuario cadastrado com sucesso, verifique seu email para confirmar o codigo!');
     } catch (e: any) {
         console.log('Error on register user: ', e);
         return formatJSONResponse(500, 'Erro ao cadastrar usuário: ' + e);
     }
 };
+
+export const confirmEmail: Handler = async (event: APIGatewayEvent): Promise<DefaultJsonResponse> => {
+    try {
+        const { USER_POOL_ID, USER_CLIENT_ID } = process.env;
+
+        if (!USER_POOL_ID || !USER_CLIENT_ID) {
+            return formatJSONResponse(500, 'Cognito Environments não encontradas');
+        }
+
+        if (!event.body) {
+            return formatJSONResponse(400, 'Parâmetros de entrada não informados');
+        }
+
+        const request = JSON.parse(event.body) as ConfirmUserEmailRequest;
+
+        const { email, code } = request;
+
+        if (!email || !email.match(emailRegex)) {
+            return formatJSONResponse(400, 'Email inválido');
+        }
+
+        if (!code || code.length !== 6) {
+            return formatJSONResponse(400, 'Código de confirmação inválido');
+        }
+
+        await new CognitoServices(USER_POOL_ID, USER_CLIENT_ID).confirmEmail(email, code);
+        return formatJSONResponse(200, 'Email confirmado com sucesso!');
+    } catch (e: any) {
+        console.log('Error on confirm user email: ', e);
+        return formatJSONResponse(500, 'Erro ao confirmar email do usuário: ' + e);
+    }
+}
--- src/services/CognitoServices.ts
+++ src/services/CognitoServices.ts
@@ -1,4 +1,4 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
+import { CognitoUser, CognitoUserPool } from "amazon-cognito-identity-js";
 
 export class CognitoServices {
     constructor(
@@ -6,15 +6,15 @@ export class CognitoServices {
         private clientId: string,
     ) { }
 
+    private poolData = {
+        UserPoolId: this.userPoolId,
+        ClientId: this.clientId,
+    };
+
     public signUp = (email: string, password: string): Promise<any> => {
         return new Promise<any>((resolve, reject) => {
             try {
-                const poolData = {
-                    UserPoolId: this.userPoolId,
-                    ClientId: this.clientId,
-                };
-
-                const userPool = new CognitoUserPool(poolData);
+                const userPool = new CognitoUserPool(this.poolData);
                 const attributeList = [];
                 userPool.signUp(
                     email, password, attributeList, attributeList,
@@ -30,5 +30,32 @@ export class CognitoServices {
                 reject(err);
             }
         });
+    }
+
+    public confirmEmail = (email: string, code: string): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            try {
+                const userPool = new CognitoUserPool(this.poolData);
+
+                const userData = {
+                    Username: email,
+                    Pool: userPool,
+                };
+
+                const user = new CognitoUser(userData);
+
+                user.confirmRegistration(code, true,
+                    (err, result) => {
+                        if (err) {
+                            reject(err.message);
+                        }
+
+                        resolve(result);
+                    }
+                );
+            } catch (err) {
+                reject(err)
+            }
+        })
     }
 }
